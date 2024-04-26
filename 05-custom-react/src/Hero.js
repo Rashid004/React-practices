8de2d0723f5cb4cc5419198b60c37f4f9cea7c33
Your Name <you@example.com>
2024-04-26 16:19:22 +0530

    Burger-corner

--- 05-custom-react/src/Hero.js
+++ 05-custom-react/src/Hero.js
@@ -1,39 +1,48 @@
 /** @format */
 
+import { useState } from "react";
+
 function Hero() {
+  const [isMenu, setIsmenu] = useState(false);
+
+  const toggleMenu = () => {
+    setIsmenu((menu) => !menu);
+  };
+
   return (
     <div>
       <header>
         <div className="container">
           <nav className="hero__nav">
-            <div className="hero__logo">
-              <div>LOGO</div>
-            </div>
-            <div className="hero_nav_list">
-              <ul>
-                <li className="hero_nav_link hero_link">HOME</li>
-                <li className="hero_nav_link hero_link">PRODUCT</li>
-                <li className="hero_nav_link hero_link">PROMO</li>
-                <li className="hero_nav_link hero_link">ABOUT</li>
-                <li className="hero_nav_link hero_link">CONTACT</li>
-              </ul>
+            <div className="hero__logo">LOGO</div>
+            <div
+              className={`menu-icon ${isMenu ? "change" : ""}`}
+              onClick={toggleMenu}
+            >
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="hero_nav_list">
+                <ul>
+                  <li className="hero_nav_link hero__link">HOME</li>
+                  <li className="hero_nav_link">PRODUCT</li>
+                  <li className="hero_nav_link">PROMO</li>
+                  <li className="hero_nav_link">ABOUT</li>
+                  <li className="hero_nav_link">CONTACT</li>
+                </ul>
+              </div>
             </div>
           </nav>
         </div>
         <div className="container">
           <div className="hero__container">
-            <h2>
-              Get Cashback <br /> Upto 50%
-            </h2>
+            <h2>Get Cashback Upto 50%</h2>
             <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the s standard dummy text ever
               since the 1500s, when an unknown printer.
             </p>
-
-            <a href="#" className="btn">
-              Order now
-            </a>
+            <button className="btn__hero__container">Order now</button>
           </div>
         </div>
       </header>
