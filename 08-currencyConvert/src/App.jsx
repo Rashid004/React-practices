ad2bbe44fdf8b9931d8780dcb9acdbfee0676819
Your Name <you@example.com>
2024-04-27 12:26:29 +0530

    Project Setup

---
+++ 08-currencyConvert/src/App.jsx
@@ -0,0 +1,16 @@
+/** @format */
+
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h2 className="text-3xl bg-orange-500">Curency convert</h2>
+    </>
+  );
+}
+
+export default App;
